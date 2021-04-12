43e5d73bdb64cad48c05e8eb84ad651d8a1ce585
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-04-12 17:51:38 +0800

    Initial commit

--- Plain Old Pasteboard/ViewController.swift
+++ Plain Old Pasteboard/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textView: UITextView!
+    
+    let DATA_KEY = "data_key"
+    var pastedStrings : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let loadedStrings = UserDefaults.standard.stringArray(forKey: DATA_KEY){
+            pastedStrings.append(contentsOf: loadedStrings)
+        }
+        showText()
     }
-
+    
+    func addText() {
+        guard let text = UIPasteboard.general.string,
+              !pastedStrings.contains(text) else {
+            return
+        }
+        
+        pastedStrings.append(text)
+        UserDefaults.standard.set(pastedStrings, forKey: DATA_KEY)
+        showText()
+    }
+    
+    func showText() {
+        textView.text = ""
+        
+        for str in pastedStrings {
+            textView.text.append("\(str)\n\n")
+        }
+    }
+    
+    @IBAction func onTrashPressed(_ sender: Any) {
+        pastedStrings.removeAll()
+        UserDefaults.standard.removeObject(forKey: DATA_KEY)
+    }
+    
 
 }
 
